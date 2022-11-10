fcbfdcab6050d52704eafc8c6194849527703d75
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2022-11-10 11:57:31 +0900

    feat(books): バリデートを追加する

--- src/models/books/dto/books.dto.ts
+++ src/models/books/dto/books.dto.ts
@@ -1,8 +1,16 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class GetBookByIdDto {
+  @IsString()
   id: string;
 }
 
 export class CreateBooksDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   author: string;
 }
