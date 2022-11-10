9ecdb5b0c344706f37e9cb2c6a815ec69d0833ee
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2022-11-11 00:41:25 +0900

    fix: 環境変数が取得できなくなっていた

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import dotenv from 'dotenv';
 
-dotenv.config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
